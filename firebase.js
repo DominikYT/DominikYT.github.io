8ad0078981cf452299f840789394d530895f9ce0
DominikYT <dominik.roblox.robux@gmail.com>
2025-10-18 10:45:39 +0200

    Initialize Firebase with configuration and auth
    Added Firebase configuration and initialization.

--- firebase.js
+++ firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
+// Your Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD6cwSZGJVq3XbH1mN09oidwr1MkfCbN9Q",
   authDomain: "dominikytgithubio-pro.firebaseapp.com",
@@ -11,6 +12,7 @@ const firebaseConfig = {
   appId: "1:997619191377:web:71b1d7367b1ab7fdbff270"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
