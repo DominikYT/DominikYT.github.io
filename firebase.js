609bffd7f89d2e9be6baffa3c08083d5608945b7
DominikYT <dominik.roblox.robux@gmail.com>
2025-10-24 20:57:25 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,16 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD6cwSZGJVq3XbH1mN09oidwr1MkfCbN9Q",
   authDomain: "dominikytgithubio-pro.firebaseapp.com",
   projectId: "dominikytgithubio-pro",
-  storageBucket: "dominikytgithubio-pro.firebasestorage.app",
+  storageBucket: "dominikytgithubio-pro.appspot.com",
   messagingSenderId: "997619191377",
   appId: "1:997619191377:web:71b1d7367b1ab7fdbff270"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
