76d876d1d401aa044d8b38d7333a7716c098ac73
DominikYT <dominik.roblox.robux@gmail.com>
2025-11-06 18:17:16 +0100

    Remove Firebase setup from firebase.js
    Removed Firebase initialization and configuration code.

--- firebase.js
+++ firebase.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD6cwSZGJVq3XbH1mN09oidwr1MkfCbN9Q",
-  authDomain: "dominikytgithubio-pro.firebaseapp.com",
-  projectId: "dominikytgithubio-pro",
-  storageBucket: "dominikytgithubio-pro.appspot.com",
-  messagingSenderId: "997619191377",
-  appId: "1:997619191377:web:71b1d7367b1ab7fdbff270"
-};
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
